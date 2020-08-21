71d36475426edb4369b601bcc72ce97655955cb3
Carl-Stanley <cds.beml@gmail.com>
2020-08-20 17:41:54 -0700

    commit

--- Challenges/2.py
+++ Challenges/2.py
@@ -1,2 +1,17 @@
 # 2. Add Two Numbers
 
+class two_num(object):
+	def to_num(self, n1):
+		n1 = 0
+		mul = 1
+		while n1 != None:
+			n1 = n1.val * mul + n1
+			mul *= 10
+			n1 = n1.next
+		return n1
+
+	def addNum(self, n1, n2):
+		return map(int, str(self.to_num(n1) + self.to_num(n2))[::-1])
+
+
+     
